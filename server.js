0194d2a796883202683ab17ae9487cec9d066cfc
Koby Whitaker <kpwhit714@gmail.com>
2023-01-31 17:53:35 -0500

    Change confusing data name

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
         console.log(err);
         return;
     }
-    var data = data;
+    var filedata = data;
 });
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -31,7 +31,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(data);
+    res.end(filedata);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
