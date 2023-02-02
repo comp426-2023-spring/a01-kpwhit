aedbd00e3114b2794ad7d827db1addd34ccb46ec
Koby Whitaker <kpwhit714@gmail.com>
2023-02-02 09:53:55 -0500

    Move console.log statement outside of server.listen() block

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ const server = http.createServer((req, res) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    // console.log(`Server listening on port ${port}`);
 });
+console.log(`Server listening on port ${port}`);
+
 // That's it! You're all done!
