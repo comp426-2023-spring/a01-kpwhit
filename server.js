7cdf9544218c0f029954b8966a9f2eb861214cb5
Koby Whitaker <kpwhit714@gmail.com>
2023-02-02 14:13:35 -0500

    Add fs.readfile in test server.js file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
+var fs = require('fs');
 var min = require('minimist');
 var args = min(process.argv.slice(2));
 const port = (args.port || 3000);
+fs.readFile('public/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('TESTING');
+    res.end(fs.data);
 });
 server.listen(port);
