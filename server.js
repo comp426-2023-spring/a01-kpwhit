e79cd87a4b26fe575e36316b6a00dba1bdd51cde
Koby Whitaker <kpwhit714@gmail.com>
2023-02-02 13:13:46 -0500

    Replace fs.data with Hello World in rs.end()

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ fs.readFile('public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(fs.data);
+    res.end("Hello World!");
+//    res.end(fs.data);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
