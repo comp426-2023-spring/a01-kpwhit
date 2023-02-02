1fe208b28cab87c8db745d7f69ddec934d136647
Koby Whitaker <kpwhit714@gmail.com>
2023-02-02 09:44:15 -0500

    Change res.end(data) to res.end(fs.data)

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ try {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(data);
+    res.end(fs.data);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
